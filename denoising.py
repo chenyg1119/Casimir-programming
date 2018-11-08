62e72d906511908962a99702ecb5e648c4d43b8f
chenyg1119 <chenyg1119@hotmail.com>
2018-11-08 15:20:43 +0100

    updated denoising.py

--- denoising.py
+++ denoising.py
@@ -1,18 +1,20 @@
 from numpy import *
 import scipy
 import pywt
-# import seaborn
 from statsmodels.robust import mad
 import matplotlib.pyplot as plt
 
 def waveletSmooth( x, wavelet="db4", level=1, title=None ):
     # calculate the wavelet coefficients
+    # returns tuple [cA,cDn,...,cD1] one approx. and details coefficients
+    # use mode "periodization", or 'per', wavelet decompose(as for wavedec) the vector. len(cA)=len(cD) = len(x)/(2**n)
     coeff = pywt.wavedec( x, wavelet, mode="per" )
     # calculate a threshold
+    # mean absolute deviation of cD_level
     sigma = mad( coeff[-level] )
-    # changing this threshold also changes the behavior,
-    # but I have not played with this very much
-    uthresh = sigma * np.sqrt( 2*np.log( len( x ) ) )
+    #threshold = \sigma*\sqrt(2*log(n)/n)
+    uthresh = sigma * np.sqrt( 2*np.log(len( x )))
+    #apply soft threshold to cD_n
     coeff[1:] = ( pywt.threshold( i, value=uthresh, mode="soft" ) for i in coeff[1:] )
     # reconstruct the signal using the thresholded coefficients
     y = pywt.waverec( coeff, wavelet, mode="per" )
